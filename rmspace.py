4b6dc4dd62233fdbee300ad3d62cc025a3a13733
Stella Gao <stella.gao.89@gmail.com>
2017-01-03 19:19:42 -0500

    Create rmspace.py

---
+++ rmspace.py
@@ -0,0 +1,36 @@
+def rmspace(fasta_file):
+	seq = ''
+	header = ''
+	
+	for line in open(fasta_file):
+		if line[0] == '>' :			
+			if seq:
+				#print header
+				with open("Human_test_rmspace.fa", "a") as f1:
+					f1.write(header) 
+					f1.write('\n')
+					f1.write(seq)
+					f1.write('\n')
+				header = ''
+				seq = ''
+			
+			header = line.rstrip()
+		
+		else:
+			if header:
+				seq += line.rstrip()
+		
+	if seq:
+		#print header
+		with open("Human_test_rmspace.fa", "a") as f1:
+			f1.write(header) 
+			f1.write('\n')
+			f1.write(seq)
+			f1.write('\n')
+
+	
+	f1.close()
+	f2.close()
+				
+				
+rmspace('Human_test.fa')
