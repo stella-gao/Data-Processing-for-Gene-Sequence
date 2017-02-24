c79fb4df45f55708793a154bf07d23930dfa8fef
Stella Gao <stella.gao.89@gmail.com>
2017-02-24 14:31:25 -0500

    Update add_head.py

--- add_head.py
+++ add_head.py
@@ -1,13 +1,13 @@
 def add_head(output_file):
 	f = open(output_file, "r")
-    lines = f.readlines()
-    f.close()
-    f = open("train_false.txt", "w")
+    	lines = f.readlines()
+    	f.close()
+    	f = open("train_false.txt", "w")
 	cnt = 0
-    for line in lines:
+    	for line in lines:
 		cnt += 1 
-        if cnt<=500 :
-            f.write("> false-sample-header " + str(cnt) + "\n")
+        	if cnt<=500 :
+            		f.write("> false-sample-header " + str(cnt) + "\n")
 			f.write(line)
 		elif cnt<=2100: 
 			f.write(line)
@@ -15,6 +15,6 @@ def add_head(output_file):
 			f.write("> false-sample-header \n")
 			f.write(line)
 
-    f.close()
+    	f.close()
 	
 add_head("train_false_1600.txt")
