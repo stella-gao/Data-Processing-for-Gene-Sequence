368963bacf0f2cf591676b917e93ae603a036e3b
Stella Gao <stella.gao.89@gmail.com>
2017-03-10 01:57:01 -0500

    Update bio_pssm.py

--- bio_pssm.py
+++ bio_pssm.py
@@ -1,3 +1,6 @@
+###############################
+## NUE-PSSM
+###############################
 from Bio import SeqIO
 from Bio import motifs
 
@@ -20,12 +23,73 @@ print max(scores)
     
 # print(m)
 # len(m)
+
+
+###############################
+## CS-PSSM
+###############################
 '''
+from Bio import SeqIO
+from Bio import motifs
+
+
 instances=[]
-for seq_record in SeqIO.parse("nue2.txt", "fasta"):
-    sq = seq_record.seq
-    instances.append(sq)
-    
-m2 = motifs.create(instances)
-score2 = m2.pssm
+
+for seq_record in SeqIO.parse("cs.txt", "fasta"):
+	sq = seq_record.seq
+	instances.append(sq)
+
+m = motifs.create(instances)
+print m.pwm
+#print scores[i]
+
+#print max(scores)
+'''
+
+
+###############################
+## NUE-PWM
+###############################
+'''
+from Bio import SeqIO
+from Bio import motifs
+
+scores = []
+
+for j in range(20):
+    scores.append(0)
+
+instances=[]
+for i in range(20):
+    for seq_record in SeqIO.parse("nue" + str(i+1) + ".txt", "fasta"):
+        sq = seq_record.seq
+        instances.append(sq)
+
+    m = motifs.create(instances)
+    scores[i] = m.pwm
+
+print max(scores)
+'''
+
+
+
+###############################
+## CS-PWM
+###############################
+'''
+from Bio import SeqIO
+from Bio import motifs
+
+
+instances=[]
+
+for seq_record in SeqIO.parse("cs.txt", "fasta"):
+	sq = seq_record.seq
+	instances.append(sq)
+
+m = motifs.create(instances)
+print m.pwm
+#print scores[i]
+
+#print max(scores)
 '''
